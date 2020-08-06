9d00867e7e3238e0dfeea8630826704d75cfd2e9
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-06 09:28:13 -0400

    Finished with types

--- functions.js
+++ functions.js
@@ -1,10 +1,8 @@
-function getSum(num1: number, num2: number): number {
+function getSum(num1, num2) {
     return num1 + num2;
 }
-
 // console.log(getSum(1, 4));
-
-let mySum = function(num1: any, num2: any): number {
+var mySum = function (num1, num2) {
     if (typeof num1 == 'string') {
         num1 = parseInt(num1);
     }
@@ -12,15 +10,12 @@ let mySum = function(num1: any, num2: any): number {
         num2 = parseInt(num2);
     }
     return num1 + num2;
-}
-
+};
 // console.log(mySum('3', 5));
-
-function getName(firstName: string, lastName: string): string {
+function getName(firstName, lastName) {
     if (lastName == undefined) {
         return firstName;
     }
-    return firstName + ' '+ lastName;
+    return firstName + ' ' + lastName;
 }
-
 console.log(getName('Mark'));
