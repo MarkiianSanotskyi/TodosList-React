7d8216bc1fb9c36e412a2828da52a8db10519745
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-08 10:38:26 -0400

    Getting started with TSC init

--- js-only.js
+++ js-only.js
@@ -1,44 +1,11 @@
-/*
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor,
-    ) {
-    let result;
-    if (typeof input1 === 'number' && input2 === 'number' || resultConversion === 'as-number') {
-       result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-
- return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Mark';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-text');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Mark', 'Anna', 'as-text');
-console.log(combinedNames);
-
-*/
-function add(n1, n2) {
-    return n1 + n2;
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-// let someValue: undefined;
+generateError('An error occured!', 500);
--- js-only.ts
+++ js-only.ts
@@ -1,53 +1,15 @@
-/*
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+let userInput: unknown;
+let userName: string;
 
+userInput = 5;
+userInput = 'Mark';
 
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor,
-    ) {
-    let result;
-    if (typeof input1 === 'number' && input2 === 'number' || resultConversion === 'as-number') {
-       result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-
- return result;
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-text');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Mark', 'Anna', 'as-text');
-console.log(combinedNames);
-
-*/
-
-function add(n1: number, n2: number): number {
-    return n1 + n2;
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
 }
 
-function printResult(num: number): void {
-    console.log('Result: ' + num);
-}
-
-printResult(add(5, 12));
-
-let combineValues: Function;
-
-combineValues = add;
-combineValues = printResult;
-
-// combineValues = 5;
-
-
-console.log(combineValues(8, 8));
-
-// let someValue: undefined;
+generateError('An error occured!', 500);
